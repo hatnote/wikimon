94f4301e1323f097a3bc4459caddfe7d9357fc11
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-04-20 13:53:34 -0700

    switch monitor to logging statements

--- monitor.py
+++ monitor.py
@@ -2,7 +2,25 @@
 from twisted.words.protocols import irc
 from twisted.internet import reactor, protocol
 import time, sys, re, datetime
- 
+
+DEBUG = False
+
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s\t%(name)s\t %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
+logger_name = __name__
+if __name__ == '__main__':
+    logger_name = 'monitor'
+
+mon_log = logging.getLogger(logger_name)
+mon_log.setLevel(logging.DEBUG)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO) 
+mon_log.addHandler(console_handler)
+
 CHANNEL = 'en.wikipedia' # use language.project
 
 PARSE_EDIT_RE = re.compile(r'(\[\[(?P<page_title>.*?)\]\])'
@@ -12,6 +30,8 @@ PARSE_EDIT_RE = re.compile(r'(\[\[(?P<page_title>.*?)\]\])'
                            r' \* (\((?P<change_size>[\+\-][0-9]+)\))?'
                            r' ?(?P<summary>.+)?')
 COLOR_RE = re.compile("\x03(?:\d{1,2}(?:,\d{1,2})?)?", re.UNICODE) # remove IRC color codes
+
+
 def process_message(message):
     no_color = COLOR_RE.sub('', message)
     ret = PARSE_EDIT_RE.match(no_color)
@@ -28,7 +48,8 @@ class Monitor(irc.IRCClient):
         self.join(self.factory.channel)
  
     def privmsg(self, user, channel, msg):
-        print process_message(msg)
+        processed = process_message(msg)
+        mon_log.info('%r', processed)
 
  
 class MonitorFactory(protocol.ClientFactory):
