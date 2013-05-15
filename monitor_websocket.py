a09a874149684cb2b82a60a73af9b54b622745e0
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-05-15 01:38:56 -0700

    enhance logging, properly broadcast without geoip for real this time

--- monitor_websocket.py
+++ monitor_websocket.py
@@ -1,6 +1,7 @@
 # some code from Twisted Matrix's irc_test.py
 from twisted.words.protocols import irc
 from twisted.internet import reactor, protocol
+from twisted.internet.protocol import ReconnectingClientFactory
 from twisted.web.client import getPage
 from autobahn.websocket import (WebSocketServerFactory,
                                 WebSocketServerProtocol,
@@ -20,6 +21,8 @@ logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s\t%(name)s\t %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
 bcast_log = logging.getLogger('bcast_log')
+irc_log = logging.getLogger('irc_log')
+api_log = logging.getLogger('api_log')
 
 
 DEFAULT_LANG = 'en'
@@ -108,14 +111,15 @@ def process_message(message, non_main_ns=NON_MAIN_NS, bcast_callback=None):
     username = msg_dict.get('user')
     is_anon = is_ip(username)
 
+    def broadcast(geo_json=None):
+        if geo_json is not None:
+            geo_dict = loads(geo_json)
+            msg_dict['geo_ip'] = geo_dict
+        bcast_callback(msg_dict)
+
     def report_failure_broadcast(error):
         bcast_log.debug("could not fetch from local geoip: %s", error)
-        broadcast(msg_dict)
-
-    def broadcast(geo_json):
-        geo_dict = loads(geo_json)
-        msg_dict['geo_ip'] = geo_dict
-        bcast_callback(msg_dict)
+        broadcast()
 
     if is_anon:
         msg_dict['is_anon'] = True
@@ -126,9 +130,8 @@ def process_message(message, non_main_ns=NON_MAIN_NS, bcast_callback=None):
             except UnicodeError:
                 pass
             else:
-                getPage(geo_url).addCallbacks(
-                    callback=broadcast,
-                    errback=report_failure_broadcast)
+                getPage(geo_url).addCallbacks(callback=broadcast,
+                                              errback=report_failure_broadcast)
     return msg_dict
 
 
@@ -137,15 +140,15 @@ class Monitor(irc.IRCClient):
         self.broadcaster = bsf
         self.non_main_ns = nmns
         self.factory = factory
-        bcast_log.info('created IRC monitor...')
+        irc_log.info('created IRC monitor...')
 
     def connectionMade(self):
         irc.IRCClient.connectionMade(self)
-        bcast_log.info('connected to IRC server...')
+        irc_log.info('connected to IRC server...')
 
     def signedOn(self):
         self.join(self.factory.channel)
-        bcast_log.info('joined %s ...', self.factory.channel)
+        irc_log.info('joined %s ...', self.factory.channel)
 
     def privmsg(self, user, channel, msg):
         try:
@@ -161,29 +164,28 @@ class Monitor(irc.IRCClient):
         self.broadcaster.broadcast(json_msg_dict)
 
 
-
-class MonitorFactory(protocol.ReconnectingClientFactory):
+class MonitorFactory(ReconnectingClientFactory):
     def __init__(self, channel, bsf, nmns=NON_MAIN_NS):
         self.channel = channel
         self.bsf = bsf
         self.nmns = nmns
 
     def buildProtocol(self, addr):
-        bcast_log.debug('Monitor IRC connected')
+        irc_log.info('monitor IRC connected to %s', self.channel)
         self.resetDelay()
         return Monitor(self.bsf, self.nmns, self)
 
     def startConnecting(self, connector):
-        bcast_log.debug('Monitor IRC starting connection')
+        irc_log.info('monitor IRC starting connection to %s', self.channel)
         protocol.startConnecting(self, connector)
 
     def clientConnectionLost(self, connector, reason):
-        bcast_log.debug('Lost Monitor IRC connection: %s', reason)
-        protocol.ReconnectingClientFactory.clientConnectionLost(self, connector, reason)
+        irc_log.error('lost monitor IRC connection: %s', reason)
+        ReconnectingClientFactory.clientConnectionLost(self, connector, reason)
 
     def clientConnectionFailed(self, connector, reason):
-        bcast_log.error('Failed Monitor IRC connection: %s', reason)
-        protocol.ReconnectingClientFactory.clientConnectionFailed(self, connector, reason)
+        irc_log.error('failed monitor IRC connection: %s', reason)
+        ReconnectingClientFactory.clientConnectionFailed(self, connector, reason)
 
 
 class BroadcastServerProtocol(WebSocketServerProtocol):
@@ -242,11 +244,12 @@ class BroadcastPreparedServerFactory(BroadcastServerFactory):
 def start_monitor(broadcaster, lang=DEFAULT_LANG, project=DEFAULT_PROJECT):
     channel = '%s.%s' % (lang, project)
     api_url = 'http://%s.%s.org/w/api.php' % (lang, project)
-    bcast_log.info('fetching namespaces from %r', api_url)
+    api_log.info('fetching namespaces from %r', api_url)
     wc = wapiti.WapitiClient('wikimon@hatnote.com', api_url=api_url)
+    api_log.info('successfully fetched namespaces from %r', api_url)
     page_info = wc.get_source_info()
     nmns = [ns.title for ns in page_info[0].namespace_map if ns.title]
-    bcast_log.info('connecting to %s...', channel)
+    irc_log.info('connecting to %s...', channel)
     f = MonitorFactory(channel, broadcaster, nmns)
     reactor.connectTCP("irc.wikimedia.org", 6667, f)
 
