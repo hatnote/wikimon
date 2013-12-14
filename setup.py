abf367133ba72427287ac04b371ddeca158272c8
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-12-14 13:19:55 -0800

    make wikimon into a real package

---
+++ setup.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+"""
+    wikimon
+    ~~~~~~~
+
+    A WebSocket-oriented monitor for streaming live changes to
+    Wikipedia. (also, wikimon, wikital monsters)
+
+    :copyright: (c) 2013 by Mahmoud Hashemi and Stephen LaPorte
+    :license: GPLv3, see LICENSE for more details.
+
+"""
+
+import sys
+from setuptools import setup
+
+
+__author__ = 'Mahmoud Hashemi and Stephen LaPorte'
+__version__ = '0.5.0'
+__contact__ = 'mahmoudrhashemi@gmail.com'
+__url__ = 'https://github.com/hatnote/wikimon'
+__license__ = 'GPLv3'
+
+desc = ('A WebSocket-oriented monitor for streaming live changes to'
+        'Wikipedia. (also, wikimon, wikital monsters)')
+
+
+if sys.version_info < (2,6):
+    raise NotImplementedError("Sorry, wikimon only supports Python >=2.6")
+
+
+if sys.version_info >= (3,):
+    raise NotImplementedError("Wikimon doesn't support Python 3 (yet!)")
+
+
+setup(name='wikimon',
+      version=__version__,
+      description=desc,
+      long_description=__doc__,
+      author=__author__,
+      author_email=__contact__,
+      url=__url__,
+      packages=['wikimon'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['wapiti', 'Twisted==13.0.0', 'autobahn==0.5.14'],
+      license=__license__,
+      platforms='any',
+      classifiers=[
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7', ]
+      )
